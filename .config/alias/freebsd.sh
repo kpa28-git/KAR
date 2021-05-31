e9d7f1040955d7ccee251d2f12867558d974755b
kevindirect <git@kevindirect.com>
2021-05-30 18:00:58 -0700

    freebsd aliases

--- .config/alias/freebsd.sh
+++ .config/alias/freebsd.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 # free bsd aliases
 
+alias fp="pkg install" \
+	fpls="pkg info" \
+	fpq="pkg info" \
+	fpg="pkg info | grep" \
+	fps="pkg search" \
+	fpu="sudo pkg update && sudo pkg upgrade";
+
